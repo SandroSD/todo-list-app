02fe14e6ee13938e8206e11f3faa833f9f23b884
Sandro <sdezerio@gmail.com>
2023-06-04 16:38:17 -0500

    delete print variables, and check auto ci.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,7 +12,7 @@ const Header: React.FC<HeaderProps> = () => {
 	return (
 		<>
 			<div className='font-serif text-4xl font-bold text-center'>
-				Todo List App
+				Todo List App Change...
 			</div>
 			<div className='my-5 h-8 flex flex-row'>
 				<div className='basis-3/4 mr-5'>
